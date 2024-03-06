706551692cc83d459deded39ff0c53e6a0b6e7a6
TOMYYYYYYYY <97633986+TOMYYYYYYYY@users.noreply.github.com>
2024-03-06 18:59:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def hello_world():
     html = f"<h1>Deployed with Zeet!!</h1>"
     return html
-
+	
+@app.route('/receive_message', methods=['POST'])
+def receive_message():
+    message = request.json.get('message')
+    # Faites quelque chose avec le message reçu
+    return "Message reçu avec succès"
+	
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=3000)
