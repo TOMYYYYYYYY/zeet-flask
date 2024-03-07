4ca101a793485d4dcd91cdeabf4ec86e1602d515
TOMYYYYYYYY <97633986+TOMYYYYYYYY@users.noreply.github.com>
2024-03-06 19:30:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
-from flask import Flask, request, abort
+#from flask import Flask, request, abort
+
+#@app.after_request
+#def add_cors_headers(response):
+    #response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:5500'
+    #response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    #response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
+    #return response
+from flask import Flask, request
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/receive', methods=['POST'])
+def receive():
+    data = request.get_json()
+    message = data.get('message')
+    print('Message received:', message)
+    return json.dumps({'status': 'success'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+#app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    html = f"<h1>Deployed with Zeet!!</h1>"
+    html = f"<h1>Deployed Zeet!!</h1>"
     return html
 	
-@app.route('/receive', methods=['POST'])
-def receive_message():
-    message = request.json.get('message')
-    # Faites quelque chose avec le message reçu
-    return "Message reçu avec succès"
+#@app.route('https://hgjhek62v7.execute-api.us-west-2.amazonaws.com/receive')
+#def receive():
+    # Your code here
+    #pass
+	
 	
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=3000)
