ca13481bf203b3047040742d4154d7db706de129
TOMYYYYYYYY <97633986+TOMYYYYYYYY@users.noreply.github.com>
2024-03-08 16:45:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/your-flask-endpoint', methods=['POST'])
+@app.route('/', methods=['POST'])
 def receive_message():
     data = request.get_json()
     message = data.get('message')
@@ -13,4 +13,4 @@ def receive_message():
     return jsonify(response_data), 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=3000)
