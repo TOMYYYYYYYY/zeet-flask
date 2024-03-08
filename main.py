20888ce2044b37150419432f47cb17936de5f8e1
TOMYYYYYYYY <97633986+TOMYYYYYYYY@users.noreply.github.com>
2024-03-08 15:50:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
-#from flask import Flask, request, abort
-
-#@app.after_request
-#def add_cors_headers(response):
-    #response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:5500'
-    #response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    #response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
-    #return response
-from flask import Flask, request
-from flask_cors import CORS
-import json
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/receive', methods=['POST'])
-def receive():
+@app.route('/your-flask-endpoint', methods=['POST'])
+def receive_message():
     data = request.get_json()
     message = data.get('message')
-    print('Message received:', message)
-    return json.dumps({'status': 'success'}), 200
+    # Process the message
+    # Example: print the message and send a response back
+    print('Received message:', message)
+    response_data = {'status': 'Message received'}
+    return jsonify(response_data), 200
 
-
-#app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    html = f"<h1>Deployed Zt!!</h1>"
-    return html
-	
-#@app.route('https://hgjhek62v7.execute-api.us-west-2.amazonaws.com/receive')
-#def receive():
-    # Your code here
-    #pass
-	
-	
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=3000)
+    app.run()
